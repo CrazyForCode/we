2020ed2072eab6e540c9fdb8416eb4d65ee4e626
Ckandzy <353223003@qq.com>
2016-12-27 02:56:30 +0800

    ???

--- We/script/game/tool.js
+++ We/script/game/tool.js
@@ -1,54 +1,57 @@
 var cross = document.getElementsByClassName("cross");
-var title = document.getElementById("title");
+var attrTitle = document.getElementById("attrTitle");
+var itemTitle = document.getElementById("itemTitle");
 var attrBar = document.getElementById("attrBar");
+var itemBar = document.getElementById("itemBar");
 var feature = document.getElementsByClassName("feature");
 var playersStatu = document.getElementById("playersStatu");
 var isDoMove = false,
-    currentTarget = '';
-
+currentTarget = '';
+itemBar.style.left = "50px";
+itemBar.style.top = "50px";
 attrBar.style.left = "50px";
 attrBar.style.top = "50px";
 playersStatu.style.left = "50px";
 playersStatu.style.top = "50px";
 
 // 状态栏
-playersStatu.onmousedown = function(e)
-{
-	if(!e)
-		e = window.event; //IE
-	posX = e.clientX - parseInt(playersStatu.style.left);
-	posY = e.clientY - parseInt(playersStatu.style.top);
+playersStatu.onmousedown = function(e) 
+{ 
+	if(!e) 
+		e = window.event; //IE 
+	posX = e.clientX - parseInt(playersStatu.style.left); 
+	posY = e.clientY - parseInt(playersStatu.style.top); 
 	isDoMove = true;
-  currentTarget = 'p';
+	currentTarget = 'p';
 	console.log(isDoMove);
-}
+} 
 playersStatu.onmouseup = function() {
 	isDoMove = false;
-	console.log(isDoMove);
-}
-
-// playersStatu.onmousemove = function mousemove(e)
-// {
-// 	console.log(isDoMove);
-// 	if(e == null)
-// 		e = window.event;//IE
-// 	if(!isDoMove) return;
-// 	playersStatu.style.left = (e.clientX - posX) + "px";
-// 	playersStatu.style.top = (e.clientY - posY) + "px";
-// }
-// 属性栏
-title.onmousedown=function(e)
-{
-	if(!e)
-		e = window.event; //IE
-	posX = e.clientX - parseInt(attrBar.style.left);
-	posY = e.clientY - parseInt(attrBar.style.top);
+} 
+attrTitle.onmousedown=function(e) 
+{ 
+	if(!e) 
+		e = window.event; //IE 
+	posX = e.clientX - parseInt(attrBar.style.left); 
+	posY = e.clientY - parseInt(attrBar.style.top); 
 	isDoMove = true;
-  currentTarget = 'a';
-}
-title.onmouseup = function() {
+	currentTarget = 'a';
+} 
+attrTitle.onmouseup = function() { 
 	isDoMove = false;
-}
+} 
+itemTitle.onmousedown=function(e) 
+{ 
+	if(!e) 
+		e = window.event; //IE 
+	posX = e.clientX - parseInt(itemBar.style.left); 
+	posY = e.clientY - parseInt(itemBar.style.top); 
+	isDoMove = true;
+	currentTarget = 'i';
+} 
+itemTitle.onmouseup = function() { 
+	isDoMove = false;
+} 
 document.onmousemove = function mousemove(e)
 {
 	if(!isDoMove) return;
@@ -62,19 +65,55 @@ document.onmousemove = function mousemove(e)
 		playersStatu.style.left = (e.clientX - posX) + "px";
 		playersStatu.style.top = (e.clientY - posY) + "px";
 	}
+	else if(currentTarget === 'i'){
+		itemBar.style.left = (e.clientX - posX) + "px";
+		itemBar.style.top = (e.clientY - posY) + "px";
+	}
 }
+// document.onmousemove = function mousemove(e) 
+// { 
+// 	if(!isDoMove) return;
+// 	if(e == null) 
+// 		e = window.event;//IE 
+// 	if(e.target === attrBar){
+// 		attrBar.style.left = (e.clientX - posX) + "px"; 
+// 		attrBar.style.top = (e.clientY - posY) + "px"; 
+// 	}
+// 	else if(e.target === playersStatu){
+// 		playersStatu.style.left = (e.clientX - posX) + "px"; 
+// 		playersStatu.style.top = (e.clientY - posY) + "px"; 
+// 	}
+
+// }
 
 // 关闭按钮
 cross[0].onclick = function(e){
-	if(!e)
-		e = window.event; //IE
+	if(!e) 
+		e = window.event; //IE 
+	// alert("hello world");
+	itemBar.style.display = "none";
+}
+cross[1].onclick = function(e){
+	if(!e) 
+		e = window.event; //IE 
 	// alert("hello world");
 	attrBar.style.display = "none";
 }
-
+// 道具栏
+feature[1].onclick = function(e){
+	if(!e) 
+		e = window.event; //IE 
+	if(itemBar.style.display === "none"){
+		itemBar.style.display = "block";
+	}
+	else{
+		itemBar.style.display = "none";
+	}
+}
+// 属性栏
 feature[2].onclick = function(e){
-	if(!e)
-		e = window.event; //IE
+	if(!e) 
+		e = window.event; //IE 
 	if(attrBar.style.display === "none"){
 		attrBar.style.display = "block";
 	}
